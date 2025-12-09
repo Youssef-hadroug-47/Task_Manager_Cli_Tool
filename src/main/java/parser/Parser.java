81aa111185c736ebdf20d3237d03034bc0388969
Hdrg-Mokni <youssef.hadroug@insat.ucar.tn>
2025-12-09 19:56:01 +0100

    structure parser

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -1,8 +1,95 @@
 package parser;
 
+import commands.Commands;
+import java.util.List;
+import java.util.ArrayList;
+import exceptions.*;
 
 public class Parser {
+    
+    private String[] args ;
+    private String MainCommand;
+    private List<String> ParsedArguments ;
 
+    //Constructor 
+    public Parser(String[] args) throws InvalidCommandException{
+        if (args==null || args.length==0)
+            throw new InvalidCommandException();
+        this.args= args;
+        MainCommand= "";
+        ParsedArguments= new ArrayList<String>(); 
+    }
+    //parsing commandline 
+    private void validAdd() throws InvalidArgumentsException {
+        if (args.length != 2)
+            throw new InvalidArgumentsException();
+        ParsedArguments.add(args[1]);
+    }
+    private void validRm() throws InvalidArgumentsException {
+        if (args.length != 2)
+            throw new InvalidArgumentsException();
+        int parsedId = Integer.parseInt(args[1]);
+        if (parsedId < 0)
+            throw new InvalidArgumentsException(args[1]);
+        ParsedArguments.add(args[1]);
+        
+    }
+    private void validLs() throws InvalidArgumentsException {
+        if (args.length!=1)
+            throw new InvalidArgumentsException();
+    }
+    private void validMarkdone() throws InvalidArgumentsException {
+        validRm();
+    }
+    private void validMarkinp() throws InvalidArgumentsException {
+        validRm();
+    }
+    private void validEdit() throws InvalidArgumentsException {
+        if (args.length!=3)
+            throw new InvalidArgumentsException();
+        int parsedId = Integer.parseInt(args[1]);
+        if (parsedId < 0)
+            throw new InvalidArgumentsException(args[1]);
+        ParsedArguments.add(args[1]);
+        ParsedArguments.add(args[2]);
+    }
+    private void validArguments(String cmd) throws InvalidArgumentsException{
+        switch (cmd) {
+            case "add" :
+                validAdd();
+                break;
+            case "rm" :
+                validRm();
+                break;
+            case "ls" :
+                validLs();
+                break;
+            case "markdone" :
+                validMarkdone();
+                break;
+            case "markinp" : 
+                validMarkinp();
+                break;
+            case "edit" :
+                validEdit();
+                break;
+        }
+    }
+    private void CommandCheck(String command) throws InvalidCommandException{
+        for (Commands arg : Commands.values())
+            if (arg.toString().equals(command)){
+                MainCommand = arg.toString();
+                return ;
+            }
 
-
+        throw new InvalidCommandException();
+    }
+    
+    public void parse() throws CommandException {
+        CommandCheck(args[0]);
+        validArguments(MainCommand);
+    }
+    public List<String> getArgs(){return new ArrayList<>(ParsedArguments);}
+    public String getCommand(){return MainCommand;}
+    
 }
