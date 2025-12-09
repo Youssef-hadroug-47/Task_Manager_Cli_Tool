467725bd07da15412f0fe3d44eedb60b2bccbdd1
Hdrg-Mokni <youssef.hadroug@insat.ucar.tn>
2025-12-09 11:02:21 +0100

    Create commands'Exception class

---
+++ src/main/java/commands/CommandException.java
@@ -0,0 +1,12 @@
+package commands ;
+
+
+public class CommandException extends Exception{
+    
+    public CommandException(){
+        System.out.println("Invalid Command");
+    }
+    public CommandException(String message ){
+        System.out.println("Invalid Command : "+message);
+    }
+}
